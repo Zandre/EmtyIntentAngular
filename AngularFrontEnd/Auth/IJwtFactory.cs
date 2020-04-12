568b93d75a9c5e479b90ce3e8d21c3ad29e1e85a
Zandre Botha <botha.zandre@gmail.com>
2020-04-12 07:39:06 +0200

    Jwt Tokens
    https://fullstackmark.com/post/13/jwt-authentication-with-aspnet-core-2-web-api-angular-5-net-core-identity-and-facebook-login

---
+++ AngularFrontEnd/Auth/IJwtFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AngularFrontEnd.Auth
+{
+    public interface IJwtFactory
+    {
+        Task<string> GenerateEncodedToken(string userName, ClaimsIdentity identity);
+    }
+}
--- AngularFrontEnd/Startup.cs
+++ AngularFrontEnd/Startup.cs
@@ -22,6 +22,11 @@ using TestProject.NewApplication5.Domain;
 using SithLordsRepository = TestProject.NewApplication5.Infrastructure.Data.SithLordsRepository;
 using TestProject.NewApplication5.Infrastructure.Data.DbContext;
 using Microsoft.AspNetCore.Identity;
+using AngularFrontEnd.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using AngularFrontEnd.Helpers;
 
 [assembly: IntentTemplate("Intent.AspNetCore.Startup", Version = "1.0")]
 [assembly: DefaultIntentManaged(Mode.Fully)]
@@ -31,6 +36,9 @@ namespace AngularFrontEnd
     [IntentManaged(Mode.Ignore)]
     public class Startup
     {
+        private const string SecretKey = "58E5FE02-C518-47AE-96AD-973AE850F120"; // todo: get this from somewhere secure
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -48,6 +56,52 @@ namespace AngularFrontEnd
             // This could automatically be done if I can get Intent to generate Unity correctly. 
             // For now manuall registration will do
             services.AddTransient<ISithLordsRepository, SithLordsRepository>();
+
+            // jwt wire up
+            // Get options from app settings
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            // Configure JwtIssuerOptions
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+
+            // api user claim policy
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiUser", policy => policy.RequireClaim(Constants.Strings.JwtClaimIdentifiers.Rol, Constants.Strings.JwtClaims.ApiAccess));
+            });
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
