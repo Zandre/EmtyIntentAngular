a036080b84f201dd064a0ce331cf6dcc3e6db798
Zandre Botha <botha.zandre@gmail.com>
2020-04-12 11:43:55 +0200

    Things build for now

--- AngularFrontEnd/ClientApp/src/app/app.module.ts
+++ AngularFrontEnd/ClientApp/src/app/app.module.ts
@@ -1,36 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { AccountModule } from './account/account.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { routing } from './app.routing';
 
 @NgModule({
    declarations: [
-      AppComponent,
-      NavMenuComponent,
-      HomeComponent,
-      CounterComponent,
-      FetchDataComponent,
-      SpinnerComponent
+    AppComponent,
+    HomeComponent
    ],
    imports: [
-      BrowserModule.withServerTransition(appId
-   ]
-}),
-    HttpClientModule,
+    AccountModule,
+    DashboardModule,
+    BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
