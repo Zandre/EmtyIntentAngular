4e7fac4d84ae9588c67351cba49e28bf682e465e
Zandre Botha <botha.zandre@gmail.com>
2020-04-25 09:30:42 +0200

    Just keep swimming

--- AngularFrontEnd/ClientApp/src/app/directives/focus.directive.ts
+++ AngularFrontEnd/ClientApp/src/app/directives/focus.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, OnInit, ElementRef, Renderer } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[tmFocus]'
 })
 export class FocusDirective implements OnInit {
 
-  constructor(private el: ElementRef, private renderer: Renderer) {
+  constructor(private el: ElementRef, private renderer: Renderer2) {
     // focus won't work at construction time - too early
 }
 
   ngOnInit() {
-    this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
+    this.renderer.selectRootElement(this.el.nativeElement);
+
+    // ZB: when moving to Renderer2
+    // https://stackoverflow.com/questions/43392607/renderer-is-deprecated-as-a-favor-for-renderer2-alternative-for-invokeelementm
+    //this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
 }
 
 }
